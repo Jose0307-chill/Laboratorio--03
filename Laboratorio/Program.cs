f9bcab6b3f97afe9fc236ce309c1b401e1ea7aac
erick-adlg <EADELEONGO@correo.url.edu.gt>
2026-02-06 22:02:37 -0600

    Update Program.cs

--- Laboratorio/Program.cs
+++ Laboratorio/Program.cs
@@ -1,9 +1,8 @@
 ﻿// Este es un comentario - será ignorado en tiempo de ejecución
-Console.WriteLine("==========================================");
+Console.WriteLine("José Méndez - 1101226");
 Console.WriteLine("Laboratorio # 03");
-// Ingresar aquí nombre y carnet del dueño del repositorio
-José Méndez - 1101226 
+// Ingresar aquí nombre y carnet del dueño del repositorio 
 // Ingresar aquí nombre y carnet del colaborador:
 
-Console.WriteLine("==========================================");
+Console.WriteLine("Erick De Leon - 1192926");
 Console.ReadLine();
