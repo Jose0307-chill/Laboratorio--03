574bf17c7e5c47c06acdfe96bcefdb8885aaeae0
Jose0307-chill <JAMENDEZLO@correo.url.edu.gt>
2026-02-11 07:46:52 -0600

    Se actualizó nombres de dueño y colaborador
    Se colocaron ambos nombres en los lugares correspondientes

--- Laboratorio/Program.cs
+++ Laboratorio/Program.cs
@@ -2,7 +2,8 @@
 Console.WriteLine("José Méndez - 1101226");
 Console.WriteLine("Laboratorio # 03");
 // Ingresar aquí nombre y carnet del dueño del repositorio 
+José Méndez - 1101226
 // Ingresar aquí nombre y carnet del colaborador:
-
+Erick de León - 1192926
 Console.WriteLine("Erick De Leon - 1192926");
 Console.ReadLine();
